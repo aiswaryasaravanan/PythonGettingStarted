3edeb549ed192d5c5c1bd64cd3bc20c7a78c4f93
Aiswarya Saravanan <aiswaryasaravanan1998@gmail.com>
2019-10-05 21:17:09 +0530

    elements removed

--- Snipets/list2.py
+++ Snipets/list2.py
@@ -3,11 +3,16 @@
 
 import re
 
+def refineList(list1):
+    pattern=re.compile("[a-zA-Z0-9_.-]+")
+
+    for element in list1:
+        if list([element])!=pattern.findall(element):
+            list1.remove(element)
+
+    return list1
+
 list1=["abc","abc1","123","_abc","ab.9i","-","abc$"]
-
-pattern=re.compile("[a-zA-Z0-9_.-]+")
-
-for element in list1:
-
-    if list([element])==pattern.findall(element):
-        print(element)
+list2=refineList(list1)
+for element in list2:
+    print(element)
