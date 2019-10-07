5ea58b77e8237fed6a5513679d50f487e902d074
Aiswarya Saravanan <aiswaryasaravanan1998@gmail.com>
2019-10-07 15:04:57 +0530

    unit test added

--- ToDos/1_csvToTxt/csvToTxt.py
+++ ToDos/1_csvToTxt/csvToTxt.py
@@ -1,7 +1,16 @@
 # read "input.txt" file and generate suggested email address and write to "output.txt" file
 
-
 import csv
+import unittest
+
+def process(row,count):
+    list1=[]
+    if count==0:
+        list1=["name","email"]
+    else:
+        list1.append(f'{" ".join([row[0],row[1]])}')
+        list1.append(row[2])
+    return list1
 
 with open("input.csv") as rCSV, open("output.csv", "w") as wCSV:
     try:
@@ -9,12 +18,7 @@ with open("input.csv") as rCSV, open("output.csv", "w") as wCSV:
         writeObj=csv.writer(wCSV)
         count=0
         for row in readObj:
-            list1=[]
-            if count==0:
-                list1=["name","email"]
-            else:
-                list1.append(f'{" ".join([row[0],row[1]])}')
-                list1.append(row[2])
+            list1=process(row,count)
             writeObj.writerow(list1)
             count=count+1
     except Exception:
@@ -22,3 +26,12 @@ with open("input.csv") as rCSV, open("output.csv", "w") as wCSV:
 
 
 
+class tesingClass(unittest.TestCase):
+
+    def test(self):
+        self.assertEqual(process(["Andro","Babu","dbs.com"],1), ["Andro Babu","dbs.com"])
+        self.assertEqual(process(["fname","lname","company website"],0), ["name","email"])
+
+
+if __name__ == '__main__':
+    unittest.main()
