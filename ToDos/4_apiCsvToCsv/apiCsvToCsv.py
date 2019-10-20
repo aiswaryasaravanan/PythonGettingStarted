43aeb6eacbceb2c1102eb361a4892625d7a0b6a7
Aiswarya Saravanan <aiswaryasaravanan1998@gmail.com>
2019-10-20 14:56:58 +0530

    Edited

--- ToDos/4_apiCsvToCsv/apiCsvToCsv.py
+++ ToDos/4_apiCsvToCsv/apiCsvToCsv.py
@@ -1,33 +1,91 @@
 import requests
 import csv
 
-res = requests.get(url="https://jsonplaceholder.typicode.com/users")
-data = res.json()
+
+def writeCSV(row):
+    writeObj.writerow(row)
+
+
+def convert(data):
+    return dict(data)
+
+
+def getValue(key):
+    return userData[key]
+
+
+def refineEmail():
+    email = getValue("email")
+    return email.casefold()
+
+
+def refinePhone():
+    phone = getValue("phone")
+    for char in badChar:
+        phone = phone.replace(char, '')
+    return phone
+
+
+def getWebsite():
+    return "https://"+getValue("website")
+
+
+def getCity():
+    return userData["address"]["city"]
+
+
+def getCompanyName():
+    return userData["company"]["name"]
+
 
 with open("input.txt", "r")as fileObj, open("output.csv", "w") as csvObj:
+
     writeObj = csv.writer(csvObj)
     fileContent = fileObj.readline()
+
     header = ["S.No", "Id", "Name", "Username", "Email",
               "City", "Phone", "Website", "Company Name"]
-    writeObj.writerow(header)
-
     badChar = ["-", "(", ")", ".", "x"]
     sNo = 0
+    URL = "https://jsonplaceholder.typicode.com/users"
+
+    writeCSV(header)
 
     while fileContent:
+        userList = []
         sNo += 1
-        for i in range(len(data)):
 
-            if data[i]["email"] == fileContent.strip():
+        PARAMS = {"email": fileContent.strip()}
+        res = requests.get(url=URL, params=PARAMS)
+        data = res.json()
 
-                phone = data[i]["phone"]
-                for char in badChar:
-                    phone = phone.replace(char, '')
+        userData = convert(data[0])
 
-                website = "https://"+data[i]["website"]
+        userList.append(sNo)
 
-                userList = [sNo, data[i]["id"], data[i]["name"], data[i]["username"], data[i]["email"].casefold(
-                ), data[i]["address"]["city"], phone, website, data[i]["company"]["name"]]
-                writeObj.writerow(userList)
-                break
+        id = getValue("id")
+        userList.append(id)
+
+        name = getValue("name")
+        userList.append(name)
+
+        username = getValue("username")
+        userList.append(username)
+
+        email = refineEmail()
+        userList.append(email)
+
+        phone = refinePhone()
+        userList.append(phone)
+
+        website = getWebsite()
+        userList.append(website)
+
+        city = getCity()
+        userList.append(city)
+
+        companyName = getCompanyName()
+        userList.append(companyName)
+
+        writeCSV(userList)
         fileContent = fileObj.readline()
